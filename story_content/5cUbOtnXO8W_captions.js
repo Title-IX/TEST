aeb384269936fba8a382e42ccc0de0f9a008613c
Title-IX <89115805+Title-IX@users.noreply.github.com>
2021-08-26 11:19:46 +0900

    Add files via upload

---
+++ story_content/5cUbOtnXO8W_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.54.25674.0%0D%0A%0D%0A00:00:00.896%20--%3E%2000:00:06.912%0D%0ALet%E2%80%99s%20begin%20with%20what%20constitutes%20sexual%20misconduct.%20Please%20click%20the%20first%20box.%0D%0A%0D%0A"}')
